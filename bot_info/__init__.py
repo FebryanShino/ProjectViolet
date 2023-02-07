cd2b1e2094f133abb90ffa49b578a6d454ef079e
FebryanS <49833582+FebryanShino@users.noreply.github.com>
2023-02-08 05:50:43 +0700

    Create __init__.py

---
+++ bot_info/__init__.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+bot_name = "ヴァイオレット"
+author_name = "FebryanS"
+bot_site = "https://Experimental-Bot-by-FebryanS.febryanshino.repl.co"
+repository = "https://github.com/FebryanShino/Violet-bot"
+bot_desc = "is a Multi-purpose Discord Bot"
+
+
+
+twitter = getenv('twitter')
+instagram = getenv('instagram')
+pixiv = getenv('pixiv')
+github = getenv('github')
+author_desc = f"[GitHub]({github})\n[Pixiv]({pixiv})\n[Twitter]({twitter})\n[Instagram]({instagram}\n\n)"
+image_url = getenv('violet_image')
