3925c000646e6c05c2711602b059ba7ea8679d55
FebryanS <49833582+FebryanShino@users.noreply.github.com>
2023-02-18 11:37:19 +0700

    Update and rename bot_info/__init__.py to Violet/bot_info.py

--- Violet/bot_info.py
+++ Violet/bot_info.py
@@ -1,12 +1,16 @@
+"""
+All Information Related to Violet
+"""
+
 from os import getenv
 
+owner = int(getenv('MY_ID'))
 bot_name = "ヴァイオレット"
 author_name = "FebryanS"
 bot_site = "https://Experimental-Bot-by-FebryanS.febryanshino.repl.co"
 repository = "https://github.com/FebryanShino/Violet-bot"
 bot_desc = "is a Multi-purpose Discord Bot"
-
-
+invite = getenv('invite')
 
 twitter = getenv('twitter')
 instagram = getenv('instagram')
@@ -14,3 +18,4 @@ pixiv = getenv('pixiv')
 github = getenv('github')
 author_desc = f"[GitHub]({github})\n[Pixiv]({pixiv})\n[Twitter]({twitter})\n[Instagram]({instagram}\n\n)"
 image_url = getenv('violet_image')
+image_url2 = getenv('violet_image2')
