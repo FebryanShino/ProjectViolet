c6e6c0d2fc31961870249bdc13f7bf41b5790936
FebryanS <49833582+FebryanShino@users.noreply.github.com>
2023-02-18 11:35:11 +0700

    Delete disappointment.py

--- disappointment.py
+++
@@ -1,3 +0,0 @@
-import random
-
-disappointment_sentence_list = ['Bruh.... Are you kidding me?', "You're such a troll", "「馬鹿みたい。。。。」", "「バカですね。。。」", "Smh humans...", "Why are you geh?","Please stop saying dumbass things\nyou're not even making sense😭"]
